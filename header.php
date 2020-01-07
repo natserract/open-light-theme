af2486baad3ebe8779f25b6f000be97a2302c796
alfinsuryaS <benjaminstwo@gmail.com>
2020-01-07 21:07:52 +0800

    Add files to repo

---
+++ header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+    <header>
+        <h1>Header</h1>
+    </header>
+    <div id="content" class="site-content">
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    get_header();
+?>
+
+<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+            <h2>Main</h2>
+</main>
+</div>
+
+<?php
+    get_sidebar();
+    get_footer();
+?>

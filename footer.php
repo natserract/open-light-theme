2af3b81fb19d14cbe849c54fb733e8de55b41ffa
alfinsuryaS <benjaminstwo@gmail.com>
2020-01-12 12:02:16 +0800

    Footer design revision(#1)

--- footer.php
+++ footer.php
@@ -1,8 +1,19 @@
 </div>
 
-   
+        <footer class="global-footer">
+			<div class="footer-item-text">
+				<span class="site-title">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                        <?php bloginfo( 'name' ); ?>
+                    </a>
+                </span>
 
-<?php wp_footer(); ?>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>" class="imprint">
+					<?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?>
+				</a>
+			</div>
+		</footer>
 
+    <?php wp_footer(); ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,15 +1,62 @@
-<?php
-    get_header();
-    get_template_part( 'template-parts/content', get_post_type() );
-?>
 
-<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-            <h2>Main</h2>
+<!-- Header --> 
+<?php get_header(); ?>
+
+<main id="main" class="site-main">
+       <header class="home-header-panel">
+            <div class="home-main-title">
+                <h1><?php echo get_bloginfo( 'name' ); ?></h1>
+                <h2>Minimal WordPress Theme</span></h2>
+                <div class="down-arrow scroll-page">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow_down.svg" alt="Scroll down">
+				</div>
+            </div>
+       </header>
+       <section class="home-intro">
+            <div class="text-chunk">
+                <p>Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique.</p>
+                <p>Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus</p> <p>Plusieurs variations de Lorem Ipsum peuvent être trouvées ici ou là, mais la majeure partie d'entre elles a été altérée par l'addition <strong><strong></strong><a href="#" target="_blank">consider making a donation to the charity.</a></strong></p>
+                <p><em>Tous les générateurs de Lorem Ipsum sur Internet tendent à reproduire le même extrait sans fin <a href="#">Contact</a> page.</em></p>
+            </div>
+            <nav class="home-intro-links">
+				<p class="home-intro-link">
+					<a href="https://github.com/alfinsuryaS/open-light-theme">About the project</a>
+				</p>
+				<p class="home-intro-link">
+					<a href="https://www.linkedin.com/in/alfinsurya/">About Author</a>
+				</p>
+				<p class="home-intro-link">
+					<a href="https://dev.to/alfinsuryas">Readers' stories</a>
+				</p>
+			</nav>
+       </section>
+       <section class="home-article-container">
+            <h1 class="story-header">Latest Posts</h1>
+            <?php 
+            if ( have_posts() ) : 
+                while ( have_posts() ) : 
+                    echo"<div class='home-article'>";
+                        the_post(); 
+
+                        echo'<a href="' . esc_url( get_permalink() ) . '">';
+                            if ( has_post_thumbnail() ) {
+                                the_post_thumbnail();
+                            };
+                        echo"</a>";
+
+                        the_title( '<h2><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
+                        the_excerpt();
+                    echo"</div>";
+                endwhile;
+                the_posts_pagination( array(
+                    'mid_size'  => 2,
+                    'prev_text' => __( 'Back', 'textdomain' ),
+                    'next_text' => __( 'Onward', 'textdomain' ),
+                ) );
+            endif; 
+            ?>
+
+       </section>
 </main>
-</div>
 
-<?php
-    get_sidebar();
-    get_footer();
-?>
+<?php get_footer(); ?>

acf0f1432e304d8b3490c473067114a235ce2181
Alfin Surya <benjaminstwo@gmail.com>
2020-01-13 09:54:03 +0800

    Update single.php

--- single.php
+++ single.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * The template for displaying all single posts
- *
- *
- * @Date:   2019-10-15 12:30:02
- * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2019-10-15 14:35:33
- * @package air-light
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- */
-
 the_post();
 
 get_header();

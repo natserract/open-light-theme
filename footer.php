c0021ddb8495abb5ff4c57b7b515bf98f09f2501
alfinsuryaS <benjaminstwo@gmail.com>
2020-01-14 21:24:02 +0800

    Fix footer design

--- footer.php
+++ footer.php
@@ -1,15 +1,14 @@
 </div>
-
         <footer class="global-footer">
 			<div class="footer-item-text">
 				<span class="site-title">
                     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                        <?php bloginfo( 'name' ); ?>
+						<?php bloginfo( 'name' ); ?> ❤️
                     </a>
                 </span>
 
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>" class="imprint">
-					<?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'openlight' ) ); ?>" class="imprint">
+					<?php printf( __( 'Proudly powered by %s', 'openlight' ), 'WordPress' ); ?>
 				</a>
 			</div>
 		</footer>

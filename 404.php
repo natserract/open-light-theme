7b0943b2e86856c55c9bd0cd7660792bbd9dae4c
alfinsuryaS <benjaminstwo@gmail.com>
2020-01-14 21:21:37 +0800

    Fix design 404 page

--- 404.php
+++ 404.php
@@ -1,23 +1,13 @@
-<?php
-    // get_header();
-?>
-<div id="primary" class="content-area">
-        <div id="content" class="site-content" role="main">
- 
-            <header class="page-header">
-                <h1 class="page-title"><?php _e( 'Not Found', 'twentythirteen' ); ?></h1>
-            </header>
- 
-            <div class="page-wrapper">
-                <div class="page-content">
-                    <h2><?php _e( 'This is somewhat embarrassing, isn’t it?', 'twentythirteen' ); ?></h2>
-                    <p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
- 
-                    <?php get_search_form(); ?>
-                </div><!-- .page-content -->
-            </div><!-- .page-wrapper -->
- 
-        </div><!-- #content -->
-    </div><!-- #primary -->
+
+<?php get_header(); ?>
+
+<section class="notfound-page post-header-panel">
+    <div class="post-header-text">
+            <h1 class="post-title">OOPS!</h1>
+            <div class="post-meta">
+				We can't find the page that you're looking for
+			</div>
+        </div>
+</section>
     
-<?php get_footer();
+<?php get_footer(); ?>
